d0b43b96980f8c670e6ee17e35ef4ccf8aaef3e0
ameenfathullah <fathullah.ameen@gmail.com>
2017-12-11 05:40:43 +0000

    first commit

---
+++ recipes/amazontimesync.rb
@@ -0,0 +1,60 @@
+#
+# Cookbook Name:: amazontimesync
+# Recipe:: amazontimesync
+#
+# Copyright 2017, arvato systems GmbH
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# uninstall ntp package if exist
+package 'ntp' do
+  action :remove
+end
+
+# install chrony package
+package 'chrony' do
+  action :install
+end
+
+# update chrony.conf file
+if node['platform_family'] == "debian"
+  template '/etc/chrony/chrony.conf' do
+    source '/home/ubuntu/chef-repo/amazontimesync/templates/default/amazontimesync/debian_chrony.conf.erb'
+    local true
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+  end
+elsif node['platform_family'] == "rhel"
+  template '/etc/chrony.conf' do
+    source '/home/ec2-user/chef-repo/amazontimesync/templates/default/amazontimesync/rhel_chrony.conf.erb'
+    local true
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+end
+elsif node['platform_family'] == "suse"
+  template '/etc/chrony.conf' do
+    source '/home/ec2-user/chef-repo/amazontimesync/templates/default/amazontimesync/suse_chrony.conf.erb'
+    local true
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+  end
+end
+
+# start chronyd service
+service 'chrony' do
+  if node['platform_family'] == "debian"
+    service_name 'chrony'  
+  elsif node['platform_family'] == "rhel"
+    service_name 'chronyd'
+  elsif node['platform_family'] == "suse"
+    service_name 'chronyd'
+  end
+  action :start
+end
